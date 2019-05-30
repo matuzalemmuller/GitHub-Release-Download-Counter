50cd391e27076086c99ed43611cb49ec5980feea
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-05-29 21:25:00 -0300

    Add download count to tags page

--- code.js
+++ code.js
@@ -5,33 +5,65 @@ let download_icon = String.fromCodePoint(0x2B07);
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 
-var get_first_ten_releases_from_api = new Promise(function (resolve, reject) {
-    let response = ""
-    let request = new XMLHttpRequest();
-    let downloads = []
+function get_first_ten_releases_from_api(){
+    return new Promise(function (resolve, reject) {
+        
+        let response = ""
+        let request = new XMLHttpRequest();
+        let downloads = []
 
-    get_versions = 'https://api.github.com/repos' + url.pathname +
-                   "?&page=1&per_page=10"
-    request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) {
-            response = JSON.parse(request.responseText);
-            for (i = 0; i < response.length; ++i) {
-                version = response[i]['name']
-                assets_json = response[i]['assets']
+        let get_versions = 'https://api.github.com/repos' + url.pathname +
+                        '?&page=1&per_page=10'
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                response = JSON.parse(request.responseText);
+                for (i = 0; i < response.length; ++i) {
+                    version = response[i]['name']
+                    assets_json = response[i]['assets']
+                    for (j = 0; j < assets_json.length; ++j) {
+                        downloads.push(assets_json[j]['download_count'])
+                    }
+                }
+                resolve(downloads)
+            } else if (request.status != 200 && request.status != 0) {
+                var reason = new Error("Status error retrieving releases from API: "
+                                    + request.status);
+                reject(reason)
+            }
+        };
+        request.open('GET', get_versions);
+        request.send();
+    });
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+function get_release_from_api(path, tag){
+    return new Promise(function (resolve,reject) {
+        let response = ""
+        let request = new XMLHttpRequest();
+        let downloads = []
+
+        let get_versions = 'https://api.github.com/repos' + path +
+                           '/tags/' + tag
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == 200) {
+                response = JSON.parse(request.responseText);
+                assets_json = response['assets']                   
                 for (j = 0; j < assets_json.length; ++j) {
                     downloads.push(assets_json[j]['download_count'])
                 }
+                resolve(downloads)
+            } else if (request.status != 200 && request.status != 0) {
+                var reason = new Error("Status error retrieving releases \
+                                       from API: " + request.status);
+                reject(reason)
             }
-            resolve(downloads)
-        } else if (request.status != 200 && request.status != 0) {
-            var reason = new Error("Status error retrieving releases from API: "
-                                   + request.status);
-            reject(reason)
-        }
-    };
-    request.open('GET', get_versions);
-    request.send();
-});
+        };
+        request.open('GET', get_versions);
+        request.send();
+    });        
+}
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 
@@ -57,37 +89,8 @@ function add_download_count_first_page(download) {
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 
-function get_release_from_api(tag){
-    return new Promise(function (resolve,reject) {
-        let response = ""
-        let request = new XMLHttpRequest();
-        let downloads = []
-
-        get_versions = 'https://api.github.com/repos' + url.pathname +
-                       "/tags/" + tag.slice(1)
-        request.onreadystatechange = function () {
-            if (request.readyState == 4 && request.status == 200) {
-                response = JSON.parse(request.responseText);
-                assets_json = response['assets']                   
-                for (j = 0; j < assets_json.length; ++j) {
-                    downloads.push(assets_json[j]['download_count'])
-                }
-                resolve(downloads)
-            } else if (request.status != 200 && request.status != 0) {
-                var reason = new Error("Status error retrieving releases \
-                                       from API: " + request.status);
-                reject(reason)
-            }
-        };
-        request.open('GET', get_versions);
-        request.send();
-    });        
-}
-
-//-----------------------------------------------------------------------------------------------------------------------------------
-
 var get_release = function(tag, i){
-    get_release_from_api(tag).then(function (fulfilled) {
+    get_release_from_api(url.pathname, tag).then(function (fulfilled) {
         releases = document.querySelectorAll('.release-entry')
 
         assets = releases[i]
@@ -106,7 +109,28 @@ var get_release = function(tag, i){
         .catch(function (error) {
             console.log(error.message);
         });
+}
 
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+var get_tag = function(tag){
+    path = '/' + pathnameArray[0] + '/' + pathnameArray[1] + '/' + pathnameArray[2]
+    get_release_from_api(path, tag).then(function (fulfilled) {
+
+        assets = document
+                .getElementsByClassName('d-flex flex-justify-between \
+                                        py-1 py-md-2 Box-body px-2')
+
+        download_count = fulfilled.slice(0)
+        for (j = 0; j < assets.length; j++) {
+            assets[j].getElementsByClassName('text-gray flex-shrink-0')
+                     .item(1).innerText += " (" + download_count.shift() + " " +
+                     download_icon + ")"
+        }
+    })
+        .catch(function (error) {
+            console.log(error.message);
+        });
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
@@ -117,25 +141,25 @@ function next_page(){
     for (i = 0; i < releases.length; i++) {
         tag = releases[i].getElementsByClassName('d-block mb-1')
                          .item(0).innerText
-        get_release(tag, i)
+        get_release(tag.slice(1), i)
     }
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 
-function tag_page(){
-    console.log("tag")
-}
-
-//-----------------------------------------------------------------------------------------------------------------------------------
-
-var first_page = function () {
-    get_first_ten_releases_from_api.then(function (fulfilled) {
+function first_page() {
+    get_first_ten_releases_from_api().then(function (fulfilled) {
         add_download_count_first_page(fulfilled)
     })
         .catch(function (error) {
             console.log(error.message);
         });
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+
+function tag_page(){
+    get_tag(pathnameArray[4])
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
